5c0e49e531c1ba8d618f1cf839a5a3e7a2389c4e
byusa123 <byusagisele@gmail.com>
2020-12-21 16:10:47 +0200

    added urls file

---
+++ display/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    url(r'^$', views.images, name='images'),
+    url(r'^home/$', views.home, name='home'),
+    url(r'^search/$', views.search_results, name='search_results'),
+    url(r'^image/(\d+)', views.image, name='image'),
+    url(r'^Silicon_Valley/', views.Silicon_Valley, name='Silicon_Valley')
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
