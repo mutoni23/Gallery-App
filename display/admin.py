b5edcaee6154fb8910a150b43dc18048e6a07488
mutoni23 <mutoniv5@gmail.com>
2020-12-22 18:26:23 +0200

    Adding images

--- display/admin.py
+++ display/admin.py
@@ -4,4 +4,4 @@ from .models import Location, Image, Category
 # Register your models here.
 admin.site.register(Location)
 admin.site.register(Image)
-# admin.site.register(Category)
+admin.site.register(Category)
